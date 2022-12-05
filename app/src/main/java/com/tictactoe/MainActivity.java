d278d1b391179bd6962b4b993b799a69fe9cd2aa
Maya Betzalel <maya.betzalel@moovit.com>
2022-12-05 17:18:45 +0200

    Finish all

--- app/src/main/java/com/tictactoe/MainActivity.java
+++ app/src/main/java/com/tictactoe/MainActivity.java
@@ -1,17 +1,16 @@
 package com.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import kotlin.reflect.KFunction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-    boolean gameActive = true;
+import com.google.android.material.button.MaterialButton;
 
-    // Player representation
-    // 0 - Xf
-    // 1 - O
+public class MainActivity extends AppCompatActivity {
+
     int activePlayer = 0;
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
@@ -19,53 +18,27 @@ public class MainActivity extends AppCompatActivity {
     // 0 - X
     // 1 - O
     // 2 - Empty cell
-    // put all win positions in a 2D array
     int[][] winPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
             {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
             {0, 4, 8}, {2, 4, 6}};
-    public static int counter = 0;
+    public int counter = 0;
 
-    // this function will be called every time a
-    // players tap in an empty box of the grid
     public void playerTap(View view) {
         ImageView img = (ImageView) view;
         int tappedImage = Integer.parseInt(img.getTag().toString());
 
-        // game reset function will be called
-        // if someone wins or the boxes are full
-        if (!gameActive) {
-            gameReset(view);
-        }
-
-        // if the tapped image is empty
         if (gameState[tappedImage] == 2) {
             ImageView currentTurnImage = findViewById(R.id.imageCurrentSituation);
-            // increase the counter
-            // after every tap
             counter++;
 
-            // check if its the last box
-            if (counter == 9) {
-                // reset the game
-                gameActive = false;
-            }
-
-            // mark this position
             gameState[tappedImage] = activePlayer;
-
-            // this will give a motion
-            // effect to the image
             img.setTranslationY(-1000f);
 
-            // change the active player
-            // from 0 to 1 or 1 to 0
             if (activePlayer == 0) {
-                // set the image of x
                 img.setImageResource(R.drawable.x);
                 activePlayer = 1;
                 currentTurnImage.setImageResource(R.drawable.oplay);
             } else {
-                // set the image of o
                 img.setImageResource(R.drawable.o);
                 activePlayer = 0;
                 currentTurnImage.setImageResource(R.drawable.xplay);
@@ -80,44 +53,50 @@ public class MainActivity extends AppCompatActivity {
                     gameState[winPosition[1]] == gameState[winPosition[2]] &&
                     gameState[winPosition[0]] != 2) {
                 flag = 1;
-
-                // game reset function be called
-                gameActive = false;
+                counter = 0;
 
                 if (gameState[winPosition[0]] == 0) {
                     resultImage.setImageResource(R.drawable.xwin);
                 } else {
                     resultImage.setImageResource(R.drawable.owin);
                 }
+                endOfGame(view);
             }
         }
         if (counter == 9 && flag == 0) {
+            counter = 0;
             resultImage.setImageResource(R.drawable.nowin);
-            gameActive = false;
+            endOfGame(view);
         }
     }
 
-    // reset the game
-    public void gameReset(View view) {
-        gameActive = true;
-        activePlayer = 0;
-        ImageView currentTurnImage = findViewById(R.id.imageCurrentSituation);
+    public void endOfGame(View view) {
+        MaterialButton resetButton = (MaterialButton)findViewById(R.id.resetGame);
+        resetButton.setVisibility(view.VISIBLE);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activePlayer = 0;
+                ImageView currentTurnImage = findViewById(R.id.imageCurrentSituation);
 
-        for (int i = 0; i < gameState.length; i++) {
-            gameState[i] = 2;
-        }
-        // remove all the images from the boxes inside the grid
-        ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
-        ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+                for (int i = 0; i < gameState.length; i++) {
+                    gameState[i] = 2;
+                }
+                resetButton.setVisibility(view.GONE);
 
-        currentTurnImage.setImageResource(R.drawable.xplay);
+                ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView4)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView6)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+
+                currentTurnImage.setImageResource(R.drawable.xplay);
+            }
+        });
     }
 
     @Override
